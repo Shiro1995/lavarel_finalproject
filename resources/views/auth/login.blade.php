f5be6d2fab94afa90361eb626ad2018504bb3348
lorence <lorence@enclave.vn>
2019-12-11 22:17:31 +0700

    Profile

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,58 +15,108 @@
     <!-- Custom Theme Style -->
     <link href="{{ URL::asset('css/custom.min.css') }}" rel="stylesheet">
     <!-- Custom Your Style -->
-    <link href="{{ URL::asset('css/style.min.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('css/login.min.css') }}" rel="stylesheet">
 </head>
 
 <body class="login">
 <div>
-    <div class="login_wrapper">
-        <div class="animate form login_form">
-            @if (session('status'))
-                <p class="alert alert-success">{{ session('status') }}</p>
-            @endif
-            <section class="login_content">
-                <form method="post" action="{{ route('login') }}">
-                    {{ csrf_field() }}
-                    <h1>Login Form</h1>
-                    <div>
-                        <input name="email" type="text" class="form-control" placeholder="Email" required=""/>
-                        @if ($errors->has('email'))
-                            <span class="help-block error">
-                                <strong>{{ $errors->first('email') }}</strong>
+        {{--<div class="animate form login_form">--}}
+            {{--@if (session('status'))--}}
+                {{--<p class="alert alert-success">{{ session('status') }}</p>--}}
+            {{--@endif--}}
+            {{--<section class="login_content">--}}
+                {{--<form method="post" action="{{ route('login') }}">--}}
+                    {{--{{ csrf_field() }}--}}
+                    {{--<h1>Login Form</h1>--}}
+                    {{--<div>--}}
+                        {{--<input name="email" type="text" class="form-control" placeholder="Email" required=""/>--}}
+                        {{--@if ($errors->has('email'))--}}
+                            {{--<span class="help-block error">--}}
+                                {{--<strong>{{ $errors->first('email') }}</strong>--}}
+                            {{--</span>--}}
+                        {{--@endif--}}
+                    {{--</div>--}}
+                    {{--<div>--}}
+                        {{--<input name="password" type="password" class="form-control" placeholder="Password" required=""/>--}}
+                        {{--@if ($errors->has('password'))--}}
+                            {{--<span class="help-block error">--}}
+                                {{--<strong>{{ $errors->first('password') }}</strong>--}}
+                            {{--</span>--}}
+                        {{--@endif--}}
+                    {{--</div>--}}
+                    {{--<div>--}}
+                        {{--<button class="btn btn-default submit" type="submit">Log in</button>--}}
+                        {{--<a class="reset_pass" href="{{ route('password.request') }}">Lost your password?</a>--}}
+                    {{--</div>--}}
+
+                    {{--<div class="clearfix"></div>--}}
+
+                    {{--<div class="separator">--}}
+                        {{--<p class="change_link">New to site?--}}
+                            {{--<a href="{{ route('register') }}" class="to_register"> Create Account </a>--}}
+                        {{--</p>--}}
+
+                        {{--<div class="clearfix"></div>--}}
+                        {{--<br/>--}}
+
+                        {{--<div>--}}
+                            {{--<h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>--}}
+                            {{--<p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</form>--}}
+            {{--</section>--}}
+        {{--</div>--}}
+    <div class="main-bg">
+        <div class="box-conatiner">
+            <div id="a">
+                <div class="circle-ripple"></div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    <h1 class="heading-left">For Continue Please Login</h1>
+                </div>
+                <div class="col-sm-6 col-md-6">
+                    <div class="wrap-login100">
+                    <span class="login100-form-title">
+                        Sign In
+                    </span>
+                        <form class="login100-form validate-form p-l-55 p-r-55 p-t-20">
+
+                            <div class="wrap-input100 validate-input m-b-16" data-validate="Please enter username">
+                                <input class="input100" type="text" name="username" placeholder="Username">
+                                <span class="focus-input100"></span>
+                            </div>
+                            <div class="wrap-input100 validate-input" data-validate="Please enter password">
+                                <input class="input100" type="password" name="pass" placeholder="Password">
+                                <span class="focus-input100"></span>
+                            </div>
+                            <div class="text-right p-t-13 p-b-23">
+                            <span class="txt1">
+                                Forgot
                             </span>
-                        @endif
-                    </div>
-                    <div>
-                        <input name="password" type="password" class="form-control" placeholder="Password" required=""/>
-                        @if ($errors->has('password'))
-                            <span class="help-block error">
-                                <strong>{{ $errors->first('password') }}</strong>
+                                <a href="#" class="txt2">
+                                    Username / Password?
+                                </a>
+                            </div>
+                            <div class="container-login100-form-btn">
+                                <button class="login100-form-btn">
+                                    Sign in
+                                </button>
+                            </div>
+                            <div class="flex-col-c p-t-140 p-b-40">
+                            <span class="txt1 p-b-9">
+                                Don’t have an account?
                             </span>
-                        @endif
+                                <a href="#" class="txt3">
+                                    Sign up now
+                                </a>
+                            </div>
+                        </form>
                     </div>
-                    <div>
-                        <button class="btn btn-default submit" type="submit">Log in</button>
-                        <a class="reset_pass" href="{{ route('password.request') }}">Lost your password?</a>
-                    </div>
-
-                    <div class="clearfix"></div>
-
-                    <div class="separator">
-                        <p class="change_link">New to site?
-                            <a href="{{ route('register') }}" class="to_register"> Create Account </a>
-                        </p>
-
-                        <div class="clearfix"></div>
-                        <br/>
-
-                        <div>
-                            <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
-                            <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
-                        </div>
-                    </div>
-                </form>
-            </section>
+                </div>
+            </div>
         </div>
     </div>
 </div>
