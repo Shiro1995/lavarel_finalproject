e9ad972dac31aeff9634dc28487bfaef51c43e15
lorence <lorence@enclave.vn>
2019-12-25 08:50:19 +0700

    Optimize code

--- app/Http/Controllers/API/AuthenticateController.php
+++ app/Http/Controllers/API/AuthenticateController.php
@@ -10,7 +10,7 @@ use Kreait\Firebase\Exception\AuthException;
 use Kreait\Firebase\Exception\FirebaseException;
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
-use Kreait\Firebase\Value\Provider;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticateController extends Controller
 {
@@ -22,19 +22,43 @@ class AuthenticateController extends Controller
 
     use HasTimestamps;
 
-    public function __construct(User $user)
-    {
+    public function __construct(User $user) {
         $account = ServiceAccount::fromJsonFile(app_path().'/Utilize/Secret/service-account.json');
         $this->firebase = (new Factory)->withServiceAccount($account)->create();
-
         $this->mModelUser = $user;
     }
 
     // https://readthedocs.org/projects/firebase-php/downloads/pdf/latest/
     public function authenticate(Request $request) {
         try {
-            $provide = Provider::FACEBOOK;
-            return $this->firebase->getAuth()->linkProviderThroughAccessToken($provide, $request->access_token);
+            $userInfo = $this->firebase->getAuth()->linkProviderThroughAccessToken($request->provider, $request->access_token);
+            if ($this->mModelUser->getByEmail($userInfo->userRecord->email) != null && $this->mModelUser->add(array([
+                    'id' => self::resetOrderInDB(),
+                    'name' => $userInfo->userRecord->displayName,
+                    'email' => $userInfo->userRecord->email,
+                    'password' => $userInfo->userRecord->passwordHash == null ? Hash::make(str_random(8)) : $userInfo->userRecord->passwordHash,
+                    'date_of_birth' => null,
+                    'gender' => null,
+                    'phone_number' => $userInfo->userRecord->phoneNumber,
+                    'avatar' => $userInfo->userRecord->photoUrl,
+                    'address' => null,
+                    'is_verified' => $userInfo->userRecord->emailVerified ? 1 : 0,
+                    'uid' => $userInfo->userRecord->uid,
+                    'created_at' => $this->freshTimestamp(),
+                    'updated_at' => $this->freshTimestamp(),
+                ])) > 0) {
+                $this->response_array = ([
+                    'http_response_code' => http_response_code(),
+                    'error' => [
+                        'code'        => 0,
+                        'message'   => 'Success'
+                    ],
+                    'data' => $this->mModelUser->getByEmail($userInfo->userRecord->email)
+                ]);
+            } else {
+                // Update user
+                \Log::info("Update");
+            }
         } catch (AuthException $e) {
             $this->response_array = ([
                 'http_response_code' => http_response_code(),
@@ -59,6 +83,19 @@ class AuthenticateController extends Controller
 
     public function pharmacy() {
 
+    }
+
+    /**
+     * Order ID in User table
+     * @return int
+     */
+    public function resetOrderInDB() {
+        $i = 1;
+        while (true){
+            if ($this->mModelUser->getById($i) == null) break;
+            $i++;
+        }
+        return $i;
     }
 }
 
